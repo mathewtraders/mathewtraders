4ae585a1a99b93b84748e5fe96ba3e7ace1ddb15
mathewtraders <66239870+mathewtraders@users.noreply.github.com>
2020-06-01 12:01:49 +0530

    Add files via upload

---
+++ mathewtraders/contact-form-handler.php
@@ -0,0 +1,17 @@
+<?PHP
+$name = $_POST['name'];
+$message = $_POST['message'];
+$email = $_POST["emailaddress"];
+$to = "anthonyveronica2000@gmail.com";
+$subject = "New Email Address for Mailing List";
+$headers = "From: $email\n";
+$message = "A visitor to your site has sent the following email address to be added to your mailing list.\n
+
+Email Address: "$email";
+$user = "$email";
+$usersubject = "Thank You";
+$userheaders = "From: anthonyveronica2000.com\n";
+$usermessage = "Thank you for subscribing to our mailing list.";
+mail($to,$subject,$message,$headers);
+mail($user,$usersubject,$usermessage,$userheaders);
+?>
